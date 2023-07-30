b5370e3e5042c7c2a913da252f314fc404a0d3fd
cicero-reis <ciceroreis@outlook.com>
2023-07-30 20:26:03 -0300

    C# Infra Data Category and Product

---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
